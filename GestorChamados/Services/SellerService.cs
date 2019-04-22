976f0a67f8d5cbec60c4585bb181966903714a1c
Fábio Borges <oibafsegrob@gmail.com>
2019-04-22 18:05:28 -0300

    Firt Create to Sellers

---
+++ GestorChamados/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using GestorChamados.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GestorChamados.Services
+{
+    public class SellerService
+    {
+        private readonly GestorChamadosContext _context;
+
+        public SellerService(GestorChamadosContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
--- GestorChamados/Startup.cs
+++ GestorChamados/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using GestorChamados.Models;
+using GestorChamados.Services;
 
 namespace GestorChamados
 {
@@ -39,6 +40,8 @@ namespace GestorChamados
             services.AddDbContext<GestorChamadosContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("GestorChamadosContext"), builder =>
 builder.MigrationsAssembly("GestorChamados")));
+
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
