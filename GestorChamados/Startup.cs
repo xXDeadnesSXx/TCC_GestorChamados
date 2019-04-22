229b53b8ec556c155d74392d007242d244614bdb
Fábio Borges <oibafsegrob@gmail.com>
2019-04-22 12:38:29 -0300

    Firt migration

--- GestorChamados/Startup.cs
+++ GestorChamados/Startup.cs
@@ -37,7 +37,8 @@ namespace GestorChamados
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<GestorChamadosContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("GestorChamadosContext")));
+                    options.UseMySql(Configuration.GetConnectionString("GestorChamadosContext"), builder =>
+builder.MigrationsAssembly("GestorChamados")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
